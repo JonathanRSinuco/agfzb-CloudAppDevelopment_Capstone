c6432083e5cdbd4b0242df2ba674a8a2fc01f9c5
Jonathan Rivera <jrivera.sinuco@gmail.com>
2023-06-22 13:43:39 -0500

    add logout

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path(route="contact/", view=views.contact, name="contact"),
     # path for registration
     path(route="login/", view=views.login, name="login"),
-    # path for logout
+    path(route="logout/", view=views.logout_request, name="logout"),
     path(route="", view=views.get_dealerships, name="index"),
     # path for dealer reviews view
     # path for add a review view
